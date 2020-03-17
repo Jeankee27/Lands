466f0ec1ac230f8f716b90a1c4e22a0d9cbd2066
Jeankee27 <62266780+Jeankee27@users.noreply.github.com>
2020-03-17 16:54:06 -0400

    Login Page News

---
+++ Land/Land/ViewsModels/LoginViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Input;
+
+namespace Land.ViewsModels
+{    
+    class LoginViewModel
+    {
+        #region Propiedades
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Constructor
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+
+        }
+        #endregion
+        #region Comandos
+        public ICommand LoginCommand 
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
--- Land/Land/ViewsModels/MainViewModel.cs
+++ Land/Land/ViewsModels/MainViewModel.cs
@@ -2,5 +2,20 @@
 {
     class MainViewModel
     {
+        #region ViewsModels
+        public LoginViewModel login 
+        {
+            get;
+            set;
+        
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel() 
+        {
+            this.login = new LoginViewModel();
+        }
+        #endregion
     }
 }
