98f7580e0859f83d6c53cd2bd1d5b0f873cbdc67
Jeankee27 <62266780+Jeankee27@users.noreply.github.com>
2020-03-17 21:06:47 -0400

    Working on BaseViewModel

---
+++ Land/Land/ViewsModels/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Land.ViewsModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnProtertyChange([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetValue<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+            {
+                return;
+            
+            }
+            backingField = value;
+            OnProtertyChange(propertyName);
+          
+        }
+
+    }
+}
--- Land/Land/ViewsModels/LoginViewModel.cs
+++ Land/Land/ViewsModels/LoginViewModel.cs
@@ -1,9 +1,22 @@
-﻿using System.Windows.Input;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Land.ViewsModels
-{    
-    class LoginViewModel
+{
+    public class LoginViewModel : BaseViewModel
     {
+
+        #region Attibutes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Propiedades
         public string Email
         {
@@ -13,32 +26,103 @@ namespace Land.ViewsModels
 
         public string Password
         {
-            get;
-            set;
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+
         }
         public bool IsRunning
         {
-            get;
-            set;
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
         }
+
         public bool IsRemembered
         {
             get;
             set;
         }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+
         #endregion
         #region Constructor
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.isEnabled = true;
 
         }
         #endregion
+
         #region Comandos
-        public ICommand LoginCommand 
+        public ICommand LoginCommand
         {
-            get;
-            set;
+            get
+            {
+                return new RelayCommand(Login);
+            }
+
+        }
+
+
+        private async void Login()
+        {
+            if (String.IsNullOrEmpty(this.Email))
+            {
+
+
+                await Xamarin.Forms.Application.Current.MainPage.DisplayAlert
+                    (
+                    "Error",
+                    "You must enter an Email",
+                    "Accept");
+                return;
+
+
+            }
+                        
+            if (String.IsNullOrEmpty(this.Password))
+            {
+
+
+                await Xamarin.Forms.Application.Current.MainPage.DisplayAlert
+                    (
+                    "Error",
+                    "You must enter an Password",
+                    "Accept");
+                return;
+
+
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "lole0310@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Xamarin.Forms.Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+
+                this.Password = string.Empty;
+                return;
+            }
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Xamarin.Forms.Application.Current.MainPage.DisplayAlert
+                ("Ok", 
+                "Fuck Yeahhhhh",
+                "Accept");
+
+
         }
         #endregion
     }
